4adb2f5750d0f7192cf69599ca8594f50e9a8499
Deepak Bhari <deepakbhari@deepaks-mbp.home>
2019-09-29 22:39:03 +0100

    Decorators on parameters and also on the class

--- decorators.ts
+++ decorators.ts
@@ -1,17 +1,39 @@
+@classDecorator
 class Boat {
 
   @testDecorator
   color: string = 'red'; //property
 
+
+  @testDecorator
   get formattedColor(): string { //accessor
     return `This boats color is ${this.color}`;
   }
+
 
   @logError('Oops boat was sunk in ocean')
   pilot(): void { //method
     throw new Error();
     console.log('swish');
   }
+
+  @logError('Oops boat was sunk in ocean')
+  captain(@parameterDecorator speed: string, @parameterDecorator generateWake: boolean): void {
+    if (speed === 'fast') {
+      console.log('swish');
+    } else {
+      console.log('nothing');
+    }
+  }
+}
+
+
+function classDecorator(constructor: typeof Boat) {
+  console.log(constructor);
+}
+
+function parameterDecorator(target: any, key: string, index: number) {
+  console.log(key, index);
 }
 
 function testDecorator(target: any, key: string) {
